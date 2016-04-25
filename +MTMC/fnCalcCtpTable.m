d11036923fd89741fad55a0ca743e2d9666ab1eb
Simon Waldman <smw13@hw.ac.uk>
2016-04-25 08:33:52 +0100

    Radically simplified Ctp table calc, removing (perhaps temoporarily) second-order correction. It works like this :-)

--- +MTMC/fnCalcCtpTable.m
+++ +MTMC/fnCalcCtpTable.m
@@ -28,36 +28,45 @@ function [ giCtp ] = fnCalcCtpTable( TurbineStruct, meanElementCSA, modeNumLayer
 %conversion using the value of Ct that corresponds to that u0 value.
 
 u0Values = TurbineStruct.giCd.GridVectors{1};
-CtValues = TurbineStruct.giCd.Values(:,1);
-alphas = MTMC.fnCalcCorrections( 0, modeNumLayersIntersected, CtValues, meanElementCSA, TurbineStruct.Diameter / 2 );
+CtValues = TurbineStruct.giCd.Values;
+alphas = MTMC.fnCalcCorrections( 0, modeNumLayersIntersected, CtValues(:,1), meanElementCSA, TurbineStruct.Diameter / 2 );
 u_cellValues = u0Values ./ sqrt(alphas');
 clear alphas;
 
-%we'll now form a griddedInterpolant of u_cell vs Ct using spline interpolation, which
-%should give us more accurate intermediate values than linear would.
+% temp stuff - simplifying radically to hopefullysee problem
 angles = TurbineStruct.giCd.GridVectors{2};
-
-giIntermediate = griddedInterpolant( {u_cellValues, angles}, TurbineStruct.giCd.Values, 'spline', 'nearest' );
-
-% MIKE doesn't do fancy interpolation, it always does linear, so we may
-% want to produce a Ctp table with more rows than the Ct table did to
-% improve accuracy. To allow for this, we *now* produce our actual table
-% from giIntermediate and calculate the Ctp values on this.
-
-NumCtpSpeeds = length(giIntermediate.GridVectors{1}) * RefineFactor;
-MinCtSpeed = giIntermediate.GridVectors{1}(1);
-MaxCtSpeed = giIntermediate.GridVectors{1}(end);
-CtpTableSpeeds = linspace(MinCtSpeed, MaxCtSpeed, NumCtpSpeeds);
-
-CtsToCorrect = giIntermediate({CtpTableSpeeds, angles});
-
-alphas = MTMC.fnCalcCorrections( 0, modeNumLayersIntersected, CtsToCorrect, meanElementCSA, TurbineStruct.Diameter / 2 );
-CtpValues = CtsToCorrect .* alphas;
-
-% so we now know the u_cell values and the Ct-prime values for our new
-% table. We'll put that into a griddedInterpolant that uses linear
-% interpolation, the same way as MIKE.
+alphas = MTMC.fnCalcCorrections( 0, modeNumLayersIntersected, CtValues, meanElementCSA, TurbineStruct.Diameter / 2 );
+CtpValues = CtValues .* alphas;
 giCtp = griddedInterpolant( {u_cellValues, angles}, CtpValues, 'linear', 'nearest' );
+
+
+% end temp stuff
+% 
+% %we'll now form a griddedInterpolant of u_cell vs Ct using spline interpolation, which
+% %should give us more accurate intermediate values than linear would.
+% angles = TurbineStruct.giCd.GridVectors{2};
+% 
+% giIntermediate = griddedInterpolant( {u_cellValues, angles}, TurbineStruct.giCd.Values, 'spline', 'nearest' );
+% 
+% % MIKE doesn't do fancy interpolation, it always does linear, so we may
+% % want to produce a Ctp table with more rows than the Ct table did to
+% % improve accuracy. To allow for this, we *now* produce our actual table
+% % from giIntermediate and calculate the Ctp values on this.
+% 
+% NumCtpSpeeds = length(giIntermediate.GridVectors{1}) * RefineFactor;
+% MinCtSpeed = giIntermediate.GridVectors{1}(1);
+% MaxCtSpeed = giIntermediate.GridVectors{1}(end);
+% CtpTableSpeeds = linspace(MinCtSpeed, MaxCtSpeed, NumCtpSpeeds);
+% 
+% CtsToCorrect = giIntermediate({CtpTableSpeeds, angles});
+% 
+% alphas = MTMC.fnCalcCorrections( 0, modeNumLayersIntersected, CtsToCorrect, meanElementCSA, TurbineStruct.Diameter / 2 );
+% CtpValues = CtsToCorrect .* alphas;
+% 
+% % so we now know the u_cell values and the Ct-prime values for our new
+% % table. We'll put that into a griddedInterpolant that uses linear
+% % interpolation, the same way as MIKE.
+% giCtp = griddedInterpolant( {u_cellValues, angles}, CtpValues, 'linear', 'nearest' );
 
 end
 
